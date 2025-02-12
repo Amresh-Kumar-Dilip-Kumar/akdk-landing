19e34a3a6a01d24dcc2b5c43f44d370e79e69afd
nk <nknapster@gmail.com>
2025-02-12 16:19:21 +0530

    move router to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter, Routes, Route } from 'react-router'
+import MainPage from './app/home-page.tsx'
+import AboutUs from './app/about-page.tsx'
+import ContactUs from './app/contact-page.tsx'
+import Service from './app/service-page.tsx'
+import Project from './app/project-page.tsx'
+import TMCD from './app/tmcd-page.tsx'
+import Privacy from './app/privacy-page.tsx'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+       <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout><MainPage /></Layout>} />
+        <Route path="/about-us" element={<Layout><AboutUs /></Layout>} />
+        <Route path="/contact-us" element={<Layout><ContactUs /></Layout>} />
+        <Route path="/service" element={<Layout><Service /></Layout>} />
+        <Route path="/project" element={<Layout><Project /></Layout>} />
+        <Route path="/tmcd" element={<Layout><TMCD /></Layout>} />
+        <Route path="/privacy" element={<Layout><Privacy /></Layout>} />
+      </Routes>
+    </BrowserRouter>
     </>
   )
 }
 
 export default App
+
+import Navbar from './components/Navbar.tsx'
+import Footer from './components/Footer.tsx'
+function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <Navbar />
+      <div className="p-12"></div>
+      {children}
+      <Footer />
+    </>
+  )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,48 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-// import App from './App.tsx'
+import App from './App.tsx'
 
-import { BrowserRouter, Routes, Route } from 'react-router'
-import MainPage from './app/home-page.tsx'
-import AboutUs from './app/about-page.tsx'
-import ContactUs from './app/contact-page.tsx'
-import Service from './app/service-page.tsx'
-import Project from './app/project-page.tsx'
-import TMCD from './app/tmcd-page.tsx'
-import Privacy from './app/privacy-page.tsx'
-import Navbar from './components/Navbar.tsx'
-import Footer from './components/Footer.tsx'
+
+
 // import ContactUs from './app/contact-page.tsx'
 
-function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <>
-      <Navbar />
-      <div className="p-12"></div>
-      {children}
-      <Footer />
-    </>
-  )
-}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout><MainPage /></Layout>} />
-        <Route path="/about-us" element={<Layout><AboutUs /></Layout>} />
-        <Route path="/contact-us" element={<Layout><ContactUs /></Layout>} />
-        <Route path="/service" element={<Layout><Service /></Layout>} />
-        <Route path="/project" element={<Layout><Project /></Layout>} />
-        <Route path="/tmcd" element={<Layout><TMCD /></Layout>} />
-        <Route path="/privacy" element={<Layout><Privacy /></Layout>} />
-      </Routes>
-    </BrowserRouter>
-
-
-    {/* <App /> */}
-
+    <App />
   </StrictMode>,
 )
