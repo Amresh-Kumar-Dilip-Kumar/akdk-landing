cda1cee5183d374814a77890a5f6ea3fb75e8b1c
Utsav Soni <utsavsoni619@gmail.com>
2025-02-12 16:11:18 +0530

    fix:links,styles

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -10,13 +10,13 @@ export default function Footer() {
                         <h3 className="text-xl font-semibold text-[#000000]">Explore</h3>
                         <ul className="mt-4 space-y-3">
                             <li>
-                                <a href="/about" className="hover:text-orange-400 transition-colors">About Us</a>
+                                <a href="/about-us" className="hover:text-orange-400 transition-colors">About Us</a>
                             </li>
                             <li>
-                                <a href="/services" className="hover:text-orange-400 transition-colors">Services</a>
+                                <a href="/service" className="hover:text-orange-400 transition-colors">Services</a>
                             </li>
                             <li>
-                                <a href="/portfolio" className="hover:text-orange-400 transition-colors">Portfolio</a>
+                                <a href="/project" className="hover:text-orange-400 transition-colors">Portfolio</a>
                             </li>
                         </ul>
                     </div>
@@ -26,7 +26,7 @@ export default function Footer() {
                         <h3 className="text-xl font-semibold ext-[#000000]">Connect</h3>
                         <ul className="mt-4 space-y-3">
                             <li>
-                                <a href="/contact" className="hover:text-orange-400 transition-colors">Contact</a>
+                                <a href="/contact-us" className="hover:text-orange-400 transition-colors">Contact</a>
                             </li>
                         </ul>
                     </div>
@@ -36,7 +36,7 @@ export default function Footer() {
                         <h3 className="text-xl font-semibold text-[#000000]">Resources</h3>
                         <ul className="mt-4 space-y-3">
                             <li>
-                                <a href="/resources" className="hover:text-orange-400 transition-colors">Menu 1</a>
+                                <a href="/" className="hover:text-orange-400 transition-colors">Menu 1</a>
                             </li>
                         </ul>
                     </div>
@@ -64,15 +64,15 @@ export default function Footer() {
                 <div className="border-t border-gray-700 my-8"></div>
 
                 {/* Bottom Section */}
-                <div className="flex flex-col md:flex-row justify-between items-center text-gray-60000 text-sm">
+                <div className="flex flex-col md:flex-row justify-between items-center text-gray-60000 text-sm  *:flex-1">
                     <p>© 2025 AKDK Digital. All rights reserved.</p>
 
-                    <div className="flex space-x-6 mt-4 md:mt-0">
+                    <div className="flex space-x-6 mt-4 md:mt-0 justify-center">
                         <a href="/tmcd" className="hover:text-orange-400 transition-colors">Terms and Conditions</a>
                         <a href="/privacy" className="hover:text-orange-400 transition-colors">Privacy Policy</a>
                     </div>
 
-                    <div className="flex space-x-6 mt-4 md:mt-0 text-2xl">
+                    <div className="flex space-x-6 mt-4 md:mt-0 text-2xl justify-end">
                         <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
                             <FaLinkedin className="cursor-pointer text-gray-600 hover:text-orange-400 transition-all" />
                         </a>
